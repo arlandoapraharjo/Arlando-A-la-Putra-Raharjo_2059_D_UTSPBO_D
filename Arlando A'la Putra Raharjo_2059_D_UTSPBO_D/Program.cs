eb3812aeb831dff535712f8624a6c28fa80af25e
Arlando A. P. Raharjo <arlandoraharjo101@gmail.com>
2026-06-10 18:56:14 +0700

    fix : resolving abstract class and composition

--- Arlando A'la Putra Raharjo_2059_D_UTSPBO_D/Program.cs
+++ Arlando A'la Putra Raharjo_2059_D_UTSPBO_D/Program.cs
@@ -1,4 +1,9 @@
 ﻿using OOPFood;
-PesananMakanan pesanan1 = new("Robby", 20, "Ayam Bakar");
 
-pesanan1.tampilInfo();
+PaketHemat pesanan1 = new PaketHemat("Robby", 20, "Ayam Bakar", 20000, 2);
+RiwayatPesanan riwayatPesanan1 = new RiwayatPesanan(pesanan1, "Paket Hemat", 2, "2024-06-01");
+riwayatPesanan1.cetakRiwayat();
+
+PaketPrasmanan prasmanan1 = new PaketPrasmanan("Anya", 10, "Gurami Bakar", 30000, 15000, 2);
+RiwayatPesanan riwayatpesanan2 = new RiwayatPesanan(prasmanan1, "Paket Prasmanan", 2, "2026-07-21");
+riwayatpesanan2.cetakRiwayat();
--- Arlando A'la Putra Raharjo_2059_D_UTSPBO_D/Resto.cs
+++ Arlando A'la Putra Raharjo_2059_D_UTSPBO_D/Resto.cs
@@ -31,7 +31,7 @@ class PaketHemat : PesananMakanan
     public int biayaPerPorsi { get; set; }
     public int jumlahPorsi { get; set; }
 
-    public PaketHemat(int biayaPerPorsi, int jumlahPorsi )
+    public PaketHemat(string namapemesan, int nomormeja, string menuutama, int biayaPerPorsi, int jumlahPorsi ) : base(namapemesan, nomormeja, menuutama)
     {
         this.biayaPerPorsi = biayaPerPorsi;
         this.jumlahPorsi = jumlahPorsi;
@@ -49,7 +49,7 @@ class PaketPrasmanan : PesananMakanan
     public int jumlahPorsi { get; set; }
     public int biayaService { get; set; }
 
-    public PaketPrasmanan(int biayaperporsi, int biayaservice, int jumlahporsi)
+    public PaketPrasmanan( string namapemesan, int nomormeja, string menuutama, int biayaperporsi, int biayaservice, int jumlahporsi) : base (namapemesan, nomormeja, menuutama)
     {
         biayaPerPorsi = biayaperporsi;
         biayaService = biayaservice;
@@ -58,17 +58,38 @@ class PaketPrasmanan : PesananMakanan
 
     public override void hitungTotalBill()
     {
-        Console.WriteLine($"Paket Prasmanan : {(jumlahPorsi * biayaPerPorsi) + biayaService} ");
+
+        Console.WriteLine($"Total Bill : {(jumlahPorsi * biayaPerPorsi) + biayaService} ");
     }
 
 }
 
 
-class RiwayatPesanan : PesananMakanan
+class RiwayatPesanan
 {
-    public string kategoriPaket { get; set; }
+    public PesananMakanan Pesanan { get; set; }
+    public string KategoriPaket { get; set; }
+    public int JumlahPorsi { get; set; }
+    public string TanggalPesan { get; set; }
+
+    public RiwayatPesanan(PesananMakanan pesanan, string kategoriPaket, int jumlahPorsi, string tanggalPesan)
+    {
+        Pesanan = pesanan;
+        KategoriPaket = kategoriPaket;
+        JumlahPorsi = jumlahPorsi;
+        TanggalPesan = tanggalPesan;
+    }
+
     public void tambahPesanan()
     {
-        Console.WriteLine($"");
+        Console.WriteLine($"Pesanan berhasil ditambahkan!");
+    }
+
+    public void cetakRiwayat()
+    {
+        Pesanan.tampilInfo();
+        Pesanan.hitungTotalBill();
+
+        Console.WriteLine($"1. {KategoriPaket} | {JumlahPorsi} porsi | {TanggalPesan} "); 
     }
 }
